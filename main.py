b49da0055712f5b4a83611560983b6eaa95047c2
Ricardo Molina <89365330+molinaricardo@users.noreply.github.com>
2023-01-02 23:22:10 -0300

    bucle
    permite mantener pantalla hasta que el usuario lo finalice

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ HEIGHT = 800
 surface = pygame.display.set_model((WIDHT, HEIGHT))
 pygame.display.set_caption('game of the live')
 
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+            sys.exit()
+    pygame.display.update()
